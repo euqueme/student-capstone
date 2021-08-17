9f1a5edbcf57209e7111f3520d29ded1fa088a02
euqueme <euqueme@gmail.com>
2021-08-17 17:12:00 -0600

    change application.js file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-import "stylesheets/application.scss"
+import "stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
