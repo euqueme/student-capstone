e881a1c1d3ddfff3e3c17ae0ecf7ce372589e224
euqueme <euqueme@gmail.com>
2021-08-18 06:45:01 -0600

    Fix config files to push to heroku

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module RorCapstone
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    config.assets.initialize_on_precompile = false
+    # config.assets.initialize_on_precompile = false
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
